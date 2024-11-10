f28d713df06638db43b21394e9dc80af05a472b8
RIKTIGATOMTEN <jath9891@gmail.com>
2024-11-10 21:44:18 +0100

    Add files via upload

---
+++ configuration/strings.lua
@@ -0,0 +1,28 @@
+Strings = {
+    not_authorized = 'Inte Auktoriserad',
+    not_authorized_desc = 'Du är inte auktoriserad att använda detta!',
+    no_one_nearby = 'Ingen Hittades',
+    no_one_nearby_desc = 'Det finns ingen i närheten att använda kryckor på!',
+    invalid_entry = 'Ogiltig Inmatning',
+    invalid_entry_desc = 'Du gjorde en ogiltig inmatning, försök igen.',
+    invalid_entry_max = 'Den maximala tiden är %s minuter!',
+    cant_wield = 'Kan Ej Användas',
+    cant_wield_desc = 'Du kan inte utrusta vapen medan du använder denna medicinska utrustning!',
+    crutch_removed = 'Kryckan Borttagen',
+    crutch_removed_desc = 'Du mår nu bättre och kryckan har tagits bort!',
+    chair_removed = 'Rullstolen Borttagen',
+    chair_removed_desc = 'Du mår nu bättre och rullstolen har tagits bort!',
+    crutch_added = 'Kryckor Utrustade',
+    crutch_added_desc = 'En professionell har rekommenderat %s minuter av användning av kryckor!',
+    chair_added = 'Rullstol Utrustad',
+    chair_added_desc = 'En professionell har rekommenderat %s minuter av användning av rullstol!',
+
+    player_id = 'Spelar-ID:',
+    select_patient = 'Spelare i Närheten',
+
+    crutch_time_dialog = 'Tid i Kryckor',
+    chair_time_dialog = 'Tid i Rullstol',
+    minutes_dialog = 'Minuter',
+
+    cheater = 'Du har blivit kickad för att fuska!',
+}
